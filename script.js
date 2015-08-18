2248aec175386cd1d7b1da86cc388226aee4753e
maksimgm <maksimmamrikov@gmail.com>
2015-08-17 23:20:36 -0700

    write out psuedo code for color paleate

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var purple = "rgb(120,81,169)";
+var red = "rgb(255,0,0)";
+var orange = "rgb(255,165,0)";
+var yellow = "rgb(255,255,0)";
+var green = "rgb(0,128,0)";
+var indigo = "rgb(75,0,130)";
+var violet = "rgb(238,130,238)";
+var color = " ";
+
 for(i=0; i<64; i++){
   var smallDiv = document.createElement('div');
   document.getElementsByTagName('body')[0].appendChild(smallDiv);
@@ -8,31 +17,34 @@ for(i=0; i<64; i++){
   smallDiv.style.borderCollapse="collapse";
   smallDiv.style.margin = "1px";
   
-  // for(i=0; i<7; i++){
-  //   var colorDiv = document.createElement('div');
-  //   document.getElementsByTagName('body')[0].appendChild(colorDiv);
-  //   smallDiv.style.width = "11.1%";
-  //   smallDiv.style.paddingBottom = "11.1%";
-  //   smallDiv.style.float = "left";
-  //   smallDiv.style.border = "1px black solid";
-  //   smallDiv.style.borderCollapse="collapse";
-  // }
+  
+
 
   smallDiv.addEventListener('click', function(){
     this.style.backgroundColor = "red";
-    // add color palete
-  //   if (true) {};
-  // });
+    // add switch statement in order to alter between colors using global var.
+    // using colorDiv
+    // 
   });
 }
 
+// switch (this){
+// //       case :
 
 
-// var purple = "rgb(120,81,169)";
-// var red = "rgb(255,0,0)";
-// var orange = "rgb(255,165,0)";
-// var yellow = "rgb(255,255,0)";
-// var green = "rgb(0,128,0)";
-// var indigo = "rgb(75,0,130)";
-// var violet = "rgb(238,130,238)";
-// var color = " ";
+for(i=0; i<7; i++){
+  var colorDiv = document.createElement('div');
+  document.getElementsByTagName('body')[0].appendChild(colorDiv);
+  smallDiv.style.width = "11.1%";
+  smallDiv.style.paddingBottom = "11.1%";
+  smallDiv.style.float = "left";
+  smallDiv.style.border = "1px black solid";
+  smallDiv.style.borderCollapse="collapse";
+  
+
+
+
+
+
+  }
+
